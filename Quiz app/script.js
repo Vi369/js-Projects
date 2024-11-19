1c9aa6fdd8964dca42e8a51b7bc8c65982b95ab3
Vikas singh varma <Vikashverma3001@gmail.com>
2024-11-19 15:54:03 +0530

    quiz app all the basic done

---
+++ Quiz app/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const questionContainer = document.getElementById('question-container');
+    const questionText = document.getElementById('question-text');
+    const optionsList = document.getElementById('options-list');
+    const showResult = document.getElementById('show-result');
+
+    // button elements
+    const startBtn = document.getElementById('start-btn');
+    const previousBtn = document.getElementById('previous-btn');
+    const nextBtn = document.getElementById('next-btn');
+    const resetBtn = document.getElementById('reset-btn');
+});
