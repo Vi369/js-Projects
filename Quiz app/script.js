c6fde14b82738db9f046450fe3db22b131b613d2
Vikas singh varma <Vikashverma3001@gmail.com>
2024-11-20 15:56:08 +0530

    quiz app completed

--- Quiz app/script.js
+++ Quiz app/script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const questionContainer = document.getElementById('question-container');
     const questionText = document.getElementById('question-text');
     const optionsList = document.getElementById('options-list');
-    const showResult = document.getElementById('show-result');
-
+    const questionContainer = document.getElementById('question-container');
+    const resultcontainer = document.getElementById('result-container')
+    const showResultDisplay = document.getElementById('show-result');
     // button elements
     const startBtn = document.getElementById('start-btn');
     const previousBtn = document.getElementById('previous-btn');
     const nextBtn = document.getElementById('next-btn');
-    const resetBtn = document.getElementById('reset-btn');
+    const resetBtn = document.getElementById('restart-btn');
 
     // quiz data
     const quizData = [
@@ -16,6 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
             question: 'What is the capital of France?',
             options: ['Paris', 'London', 'Berlin', 'Rome'],
             answer: 'Paris',
+        },
+        {
+            question: "Which planet is known as the Red Planet?",
+            options: ["Mars", "Venus", "Jupiter", "Saturn"],
+            answer: "Mars",
+        },
+          {
+            question: "Who wrote 'Hamlet'?",
+            options: [
+              "Charles Dickens",
+              "Jane Austen",
+              "William Shakespeare",
+              "Mark Twain",
+            ],
+            answer: "William Shakespeare",
         },
         {
             question: 'What is the capital of Italy?',
@@ -38,8 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // function to start the quiz
     function startQuiz() {
         startBtn.classList.add('hidden');
-        previousBtn.classList.remove('hidden');
-        nextBtn.classList.remove('hidden');
+        questionContainer.classList.remove('hidden')    
         showQuestion();
     }
 
@@ -48,8 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const currentQuestion = quizData[currentQuestionIndex];
         questionText.textContent = currentQuestion.question;
 
+        if(currentQuestionIndex <= 0){
+            previousBtn.disabled = true;
+        }else{
+            previousBtn.disabled = false;
+        }
         // showing the options
-        optionsList.innerHTML = ''; // make sure privious options are removed
+        optionsList.innerHTML = " " // make sure privious options are removed
         currentQuestion.options.forEach((option, index) => {
             const li = document.createElement('li');
             li.textContent = `${index + 1}. ${option}`;
@@ -58,8 +77,57 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     }
 
-    // selected answer
+    // selected answer function
     function selectedAnswer(option) {
-        
+        console.log(option)
+        const correctAnswer = quizData[currentQuestionIndex].answer;
+        if(option === correctAnswer){
+            score++;
+        }
     }
+
+    // function show result
+    function showResult(){
+        questionContainer.classList.add('hidden')
+        resultcontainer.classList.remove('hidden')
+
+        showResultDisplay.textContent = `${score} out of ${quizData.length}`;
+    }
+
+    // previous button event
+    previousBtn.addEventListener('click', () => previousQuestion());
+
+    // previous question function
+    function previousQuestion(){
+        if(currentQuestionIndex > 0){
+            previousBtn.disabled = false;
+            currentQuestionIndex--;
+            showQuestion();
+        }
+    }
+
+    // next question button event
+    nextBtn.addEventListener('click', nextQuestion)
+
+    // next question function
+    function nextQuestion(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < quizData.length){
+            showQuestion();
+        }else{
+            showResult();
+        }
+    }
+
+   // restart button event
+  resetBtn.addEventListener('click', ()=> resetQuiz());
+
+  // restart quiz function 
+  function resetQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    resultcontainer.classList.add('hidden');
+    startQuiz();
+  }
+
 });
