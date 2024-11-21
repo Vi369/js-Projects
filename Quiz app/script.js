9ae4a4e7cbfa428543c780d415fdd90e277e1665
Vikas singh varma <Vikashverma3001@gmail.com>
2024-11-21 12:55:56 +0530

    add features only at a time one answer selected

--- Quiz app/script.js
+++ Quiz app/script.js
@@ -49,49 +49,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let score = 0;
 
     startBtn.addEventListener('click', startQuiz);
-
     // function to start the quiz
     function startQuiz() {
         startBtn.classList.add('hidden');
         questionContainer.classList.remove('hidden')    
         showQuestion();
-    }
-
-    // function to show the question
-    function showQuestion() {
-        const currentQuestion = quizData[currentQuestionIndex];
-        questionText.textContent = currentQuestion.question;
-
-        if(currentQuestionIndex <= 0){
-            previousBtn.disabled = true;
-        }else{
-            previousBtn.disabled = false;
-        }
-        // showing the options
-        optionsList.innerHTML = " " // make sure privious options are removed
-        currentQuestion.options.forEach((option, index) => {
-            const li = document.createElement('li');
-            li.textContent = `${index + 1}. ${option}`;
-            li.addEventListener('click', () => selectedAnswer(option));
-            optionsList.appendChild(li);
-    })
-    }
-
-    // selected answer function
-    function selectedAnswer(option) {
-        console.log(option)
-        const correctAnswer = quizData[currentQuestionIndex].answer;
-        if(option === correctAnswer){
-            score++;
-        }
-    }
-
-    // function show result
-    function showResult(){
-        questionContainer.classList.add('hidden')
-        resultcontainer.classList.remove('hidden')
-
-        showResultDisplay.textContent = `${score} out of ${quizData.length}`;
     }
 
     // previous button event
@@ -108,7 +70,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // next question button event
     nextBtn.addEventListener('click', nextQuestion)
-
     // next question function
     function nextQuestion(){
         currentQuestionIndex++;
@@ -119,15 +80,62 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-   // restart button event
-  resetBtn.addEventListener('click', ()=> resetQuiz());
+    // restart button event
+    resetBtn.addEventListener('click', ()=> resetQuiz());
 
-  // restart quiz function 
-  function resetQuiz(){
+    // restart quiz function 
+    function resetQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     resultcontainer.classList.add('hidden');
     startQuiz();
-  }
+    }
+
+    // function to show the question
+    function showQuestion() {
+        const currentQuestion = quizData[currentQuestionIndex];
+        questionText.textContent = currentQuestion.question;
+
+        if(currentQuestionIndex <= 0){
+            previousBtn.disabled = true;
+        }else{
+            previousBtn.disabled = false;
+        }
+        // showing the options
+        optionsList.innerHTML = " " // make sure privious options are removed
+        currentQuestion.options.forEach((option, index) => {
+            const li = document.createElement('li');
+            li.setAttribute('id', `option${index}`);
+            li.textContent = `${index + 1}. ${option}`;
+            li.addEventListener('click', () => selectedAnswer(option, index));
+            optionsList.appendChild(li);
+    })
+    }
+
+    // selected answer function
+    function selectedAnswer(option, index) {
+        const options = optionsList.children;
+        // remove selected class from all options
+        for(let i = 0; i < options.length; i++){
+            options[i].classList.remove('selected');
+        }
+
+        // add selected class to the selected option
+        document.getElementById(`option${index}`).classList.add('selected');
+        
+        const correctAnswer = quizData[currentQuestionIndex].answer;
+        if(option === correctAnswer){
+            score++;
+            console.log(score)
+        }
+    }
+
+    // function show result
+    function showResult(){
+        questionContainer.classList.add('hidden')
+        resultcontainer.classList.remove('hidden')
+
+        showResultDisplay.textContent = `${score} out of ${quizData.length}`;
+    }
 
 });
